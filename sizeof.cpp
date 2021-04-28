13b3961af57ee895a64b1876cf9d48ce3b8d5e66
07Irshad <80592277+07Irshad@users.noreply.github.com>
2021-04-28 06:56:23 +0530

    size of int char.cpp

---
+++ sizeof.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int integer;
+	char character;
+	float floatt;
+	double doublee;
+
+	cout << "Size of int is:    " << sizeof(integer) <<"\n";
+	cout << "Size of char is:   " << sizeof(character) <<"\n";	
+	cout << "Size of float is:  " << sizeof(floatt) <<"\n";
+	cout << "Size of double is: " << sizeof(doublee) <<"\n";
+	return 0;
+}
