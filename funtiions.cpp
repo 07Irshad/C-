49b1f669f0d99494da54b25be8855ad4131b5b3f
07Irshad <80592277+07Irshad@users.noreply.github.com>
2021-04-28 07:16:11 +0530

    funtiions.cpp

---
+++ funtiions.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+bool prime(int n);
+int main() 
+{
+    int n,i;
+    bool flag=false;
+    cout << "Enter a positive  integer: ";
+    cin >> n;
+    for(i=2;i<=n/2;++i) 
+	{
+		if (prime(i)) 
+		{
+			if (prime(n-i)) 
+			{
+				cout << n << " = " << i << " + " << n-i << endl;
+                flag = true;
+            }
+        }
+    }
+
+    if (!flag)
+    cout << n << "Not possible";
+    return 0;
+}
+
+
+bool prime(int n)
+{
+    int i;
+    bool p = true;
+    if (n==0||n==1) 
+	{
+        p = false;
+    }
+    else 
+	{
+		for(i=2;i<=n/2;++i) 
+		{
+            if(n%i==0) 
+			{
+                p = false;
+                break;
+            }
+        }
+    }
+    return p;
+}
